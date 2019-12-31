f1cec718ee80c85f184294e1169a7580997e3465
desham25 <runwiththebull84@gmail.com>
2019-12-31 02:13:33 -0500

    google map

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -1,4 +1,27 @@
+
+
+
+
+
+
+
 AOS.init({
      offset: 100,
      duration: 1500
+});
+
+$(function () {
+     $('.owl-carousel.testimonial-carousel').owlCarousel({
+          nav: true,
+          navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+          dots: false,
+          responsive: {
+               0: {
+                    items: 1,
+               },
+               750: {
+                    items: 2,
+               }
+          }
+     });
 });
